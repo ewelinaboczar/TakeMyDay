18603ef3c77adb7e18b225a2edffd2aab71a250e
ewelinaboczar <ewelina.boczar@student.pk.edu.pl>
2021-11-12 20:16:54 +0100

    routing is working

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "Projekcik Ewelina Boczar ;) #";
-$variable_name=1;
-echo $variable_name;
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'],'/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index','DefaultController');
+Routing::get('projects', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function index()
+    {
+        $this->render('login');
+    }
+
+    public function projects()
+    {
+        $this->render('projects');
+    }
+}
