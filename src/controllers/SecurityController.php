c026da77bca6fb19c6d3ca699dc3b803364eab46
ewelinaboczar <ewelina.boczar@student.pk.edu.pl>
2022-01-02 23:59:43 +0100

    add user table

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,16 +2,23 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController{
     public function login(){
-        $user = new User("ew@gmail.com","abcd","ew986","Poland");
+        $userRepository = new UserRepository();
 
         if(!$this->isPost()){
             return $this->render('login');
         }
+
         $email = $_POST["user_name_or_email"];
         $password = $_POST["password"];
+        $user = $userRepository->getUser($email);
+
+        if (!$user){
+            return $this->render('login',['messages'=>['User not exist!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login',['messages'=>['User with this email not exist!']]);
