3e7e4a109f25af4a27f39534c3adcb9afda8313b
ewelinaboczar <ewelina.boczar@student.pk.edu.pl>
2022-01-07 22:25:39 +0100

    add validate script to registration page

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+const form = document.querySelector("form");
+const emailInput = form.querySelector('input[name="email"]');
+const confirmedPasswordInput = form.querySelector('input[name="confirm_password"]');
+
+function register() {
+    document.getElementById("register_id").submit();
+}
+
+function isEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+function arePasswordsSame(password, confirmedPassword){
+    return password === confirmedPassword;
+}
+
+function markValidation(elem, condition) {
+    !condition ? elem.classList.add('no-valid') : elem.classList.remove('no-valid');
+}
+
+function emailValidation () {
+    setTimeout(function () {
+            console.log("emailValidate");
+            markValidation(emailInput, isEmail(emailInput.value));
+        },
+        1000
+    );
+}
+function passwordValidation (){
+    setTimeout(function () {
+            const condition = arePasswordsSame(
+                confirmedPasswordInput.previousElementSibling.value,
+                confirmedPasswordInput.value
+            );
+            markValidation(confirmedPasswordInput, condition);
+        },
+        1000
+    );
+}
+
+emailInput.addEventListener('keyup', emailValidation);
+confirmedPasswordInput.addEventListener('keyup', passwordValidation);
