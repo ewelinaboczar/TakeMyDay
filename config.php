97e8ef8b74d29d0847661605585b7ddae8c7b17f
ewelinaboczar <72562136+ewelinaboczar@users.noreply.github.com>
2022-02-03 22:23:12 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'xepdcjxtjqfwiq';
-const PASSWORD = '3940db8de18940daa668a86f3c037a29730efe8b3ae9c03a377b63559f64c885';
-const HOST = 'ec2-52-49-56-163.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'dg4ilrn07s1ej';
